5af57b8631b26d28203154ce7c58abe50c58a512
Estefanio NS <estefanions@gmail.com>
2018-03-11 21:57:56 -0300

    change app 'init' to 'relevant'

--- web/index.php
+++ web/index.php
@@ -6,8 +6,9 @@ require '../vendor/autoload.php';
 
 
 
-$app = new Rubricate\Init\ApplicationInit(
-    new Rubricate\Init\ControllerNamespaceInit('App.Controller')
+$app = new Rubricate\Relevant\ApplicationRelevant(
+    new Rubricate\Relevant\ControllerNamespaceRelevant('App.Controller'),
+    new Rubricate\Uri\Uri()
 );
 
 $app->run();
