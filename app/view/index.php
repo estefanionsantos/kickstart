52198e4ac95ac0a2bda77c9cae02cfdec6ba3f32
Estefanio N Santos <estefanions@gmail.com>
2021-05-09 21:27:43 -0300

    update app/view/index.php

--- app/view/index.php
+++ app/view/index.php
@@ -2,28 +2,63 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8"/>
-    <title>Welcome to Rubricate PHP</title>
+    <title>Start Kick - Ens</title>
+    <link rel="stylesheet" href="asset/dbk/css/reset.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="asset/dbk/css/style.min.css" type="text/css" media="all" />
     <link rel="stylesheet" href="asset/css/custom.css" type="text/css" media="all" />
 </head>
 <body>
-<div class="content boxShadow">
 
-    <div class="header_page">
-        <h1>Rubricate PHP</h1>
-    </div>
-   
-    <div class="main">
-        <p>Welcome!</p>
-    </div>
-        
-    <div class="expand">
-        <p style="text-align: right"> 
-            <a target="_blank" href="http://rubricate.github.io/">
-                rubricate.github.io
-            </a> 
+
+<div class="expand bg_prm mb_1">
+<div class="container">
+<input type="checkbox" id="mn_x_bt" />
+<label for="mn_x_bt">&#9776;</label>
+<div class="mn_x mn_prm">
+    <ul>
+    <li><a href="#">Home</a></li>
+    <li>
+        <a href="#">Products</a>
+        <ul>
+            <li><a href="#">Product 1</a></li>
+            <li><a href="#">Product 2</a></li>
+            <li> 
+                <a href="#">Product 3</a> 
+                <ul>
+                      <li><a href="#">Sub Product 1</a></li>
+                      <li><a href="#">Sub Product 2</a></li>
+                      <li><a href="#">Sub Product 3</a></li>
+                </ul>
+           </li>
+        </ul>
+      </li>
+      <li><a href="https://dobokcss.github.io">DobokCSS</a></li>
+      <li><a href="https://rubricate.github.io">RubricatePHP</a></li>
+</ul>
+</div>
+</div>
+</div>
+
+<div class="container" id="ct_container">
+
+<div class="row">
+    <div class="gr gr_12">
+        <h1>Start Kick</h1>
+        <p>
+            Template with only the main menu to start the project from scratch
         </p>
     </div>
+</div>
 
-</div>    
+</div>
+
+<footer class="fold">
+   <div class="container">
+        <ul class="lt_x fl_r">
+            <li>by: <a href="https://estefanions.github.io" class="lk_prm">ens</a></li>
+        </ul>
+    </div>
+</footer>
+
 </body>
 </html> 
